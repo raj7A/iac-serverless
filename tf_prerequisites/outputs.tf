c091041919c25005b5c4d6e62dd801cbd240f9dd
anand.raj <anand.raj>
2024-11-07 20:39:15 +0530

    tf backend | changes to create s3 state store bucket through modules

--- tf_prerequisites/outputs.tf
+++ tf_prerequisites/outputs.tf
@@ -5,5 +5,5 @@ output "tf_lock_arn" {
 
 output "tf_state_arn" {
   description = "arn of s3 bucket used to store terraform state"
-  value       = aws_s3_bucket.s3_tf_state_bucket.arn
+  value       = module.s3_bucket.s3_bucket_arn
 }
--- tf_prerequisites/s3.tf
+++ tf_prerequisites/s3.tf
@@ -1,57 +1,26 @@
-resource "aws_s3_bucket" "s3_tf_state_bucket" {
-  bucket        = format("%s-tfstate", var.prefix)
-  force_destroy = true
-
-
-  lifecycle {
-    prevent_destroy = true
+module "s3_bucket" {
+  source                   = "terraform-aws-modules/s3-bucket/aws"
+  version                  = "4.2.1"
+  bucket                   = format("%s-tfstate", var.prefix)
+  force_destroy            = true
+  object_ownership         = "BucketOwnerPreferred"
+  control_object_ownership = true
+  acl                      = "private"
+  block_public_acls        = true
+  block_public_policy      = true
+  ignore_public_acls       = true
+  restrict_public_buckets  = true
+  versioning = {
+    enabled = true
+  }
+  server_side_encryption_configuration = {
+    rule = {
+      sse_algorithm = "AES256"
+    }
   }
 
   tags = {
     Name = format("%s-tf-state-bucket", var.prefix)
   }
 
-}
-
-resource "aws_s3_bucket_versioning" "s3_tf_state_bucket_versioning" {
-  bucket = aws_s3_bucket.s3_tf_state_bucket.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "s3_tf_state_bucket_encryption" {
-  bucket = aws_s3_bucket.s3_tf_state_bucket.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-
-resource "aws_s3_bucket_ownership_controls" "s3_tf_state_bucket_oc" {
-  bucket = aws_s3_bucket.s3_tf_state_bucket.id
-
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-resource "aws_s3_bucket_acl" "s3_tf_state_bucket_acl" {
-  depends_on = [aws_s3_bucket_ownership_controls.s3_tf_state_bucket_oc]
-
-  bucket = aws_s3_bucket.s3_tf_state_bucket.id
-  acl    = "private"
-
-}
-
-resource "aws_s3_bucket_public_access_block" "s3_tf_state_public_access_block" {
-  bucket = aws_s3_bucket.s3_tf_state_bucket.bucket
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
 }
