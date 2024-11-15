7f22f0645308d1cca4e818e1e92ff732f7b7809d
anand.raj <anand.raj>
2024-11-15 21:36:36 +0530

    Apigateway module | Initialize

---
+++ modules/apigateway/apigateway.tf
@@ -0,0 +1,4 @@
+module "apigateway-v2" {
+  source  = "terraform-aws-modules/apigateway-v2/aws"
+  version = "5.2.0"
+}
---
+++ modules/apigateway/variables.tf
@@ -0,0 +1,5 @@
+variable "prefix" {
+  type        = string
+  description = "prefix used for naming resources"
+  default     = "iac-serverless-raj"
+}
