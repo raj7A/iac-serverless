cd95e7b5676839de7578149861c8cd0fd8206a75
anand.raj <anand.raj>
2024-11-18 03:48:52 +0530

    S3 object | Trigger updates on file changes

--- modules/s3/s3.tf
+++ modules/s3/s3.tf
@@ -24,10 +24,10 @@ module "s3_bucket" {
 
 resource "aws_s3_object" "user_app_static_page_objects" {
   for_each = fileset("./application/user-app/static-page/", "*")
-
-  bucket = module.s3_bucket.s3_bucket_id
-  key    = each.value
-  source = "./application/user-app/static-page/${each.value}"
+  bucket   = module.s3_bucket.s3_bucket_id
+  key      = each.value
+  source   = "./application/user-app/static-page/${each.value}"
+  etag     = filemd5("./application/user-app/static-page/${each.value}")
 
   tags = {
     Name = format("%s-%s", var.prefix, "user-app-static-page")
